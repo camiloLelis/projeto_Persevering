c08cb202b2a791f5117c134fca6f9dbabbb39c9e
camilo <lafirmafuerte@gmail.com>
2024-01-17 11:15:28 -0300

    feat(usuário): separação de responsabilidades em controllers, models e services

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,12 +1,33 @@
 // services/UserService.js
-import { getUserByIdModel } from '../models/UserModels.js';
+import { getUserByIdModel, postUserModels, usersAllModel, updateUserModel, deleteUserModel } from '../models/UserModels.js';
 
-const getUserByIdService = async (userId) => {
-  console.log("teste userservice");
-  const x = getUserByIdModel(userId)
-  return x;
+const getUserByIdService = (userId) => {
+  return getUserByIdModel(userId); 
 };
+
+const postUserService = async (nome, email, senha) => {
+   const { nome: usuario } = await postUserModels(nome, email, senha);
+   return usuario
+}
+
+const usersAllService = () => { 
+  return usersAllModel();
+}
+
+const updateUserService = async (userId, nome, email, senha) => {
+  const { nome: usuario } =  await updateUserModel(userId, nome, email, senha);
+  return usuario;
+}
+
+const deleteUserService = async (userId) => {
+  const { nome } = await deleteUserModel(userId);
+  return nome;
+}
 
 export default {
   getUserByIdService,
+  postUserService,
+  usersAllService,
+  updateUserService,
+  deleteUserService
 };
