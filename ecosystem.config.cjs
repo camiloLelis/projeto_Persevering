0e5d0379304b6d3f66b319e12e3fe323301f0e16
camilo <lafirmafuerte@gmail.com>
2024-02-09 13:06:13 -0300

    add arquivos ignorados

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
         instances: 1,
         autorestart: true,
         watch: true,
-        ignore_watch: ['./prisma/dev.db','.git', '.gitignore', 'dev.db-journal'],
+        ignore_watch: ['/prisma/dev.db','.git', '.gitignore', 'prisma/dev.db-journal','prisma/', './prisma'],
         max_memory_restart: '1G',
         env: {
           NODE_ENV: 'development',
